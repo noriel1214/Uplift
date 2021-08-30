28e2ab8c918dcd422d31219f5ce19ce79e251d14
Noriel <norielsg1214@gmail.com>
2021-08-30 14:33:42 -0400

    Section 8 - AddToCart Action

---
+++ Uplift.Utility/SD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uplift.Utility
+{
+    public class SD
+    {
+        public const string SessionCart = "Cart";
+    }
+}
